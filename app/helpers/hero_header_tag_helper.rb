008aaa60fb743823f6becd058f23c30b4c317129
Djouko <destructeurkratos@gmail.com>
2026-03-20 09:06:12 +0000

    fix: image upload banner9 and logs

--- app/helpers/hero_header_tag_helper.rb
+++ app/helpers/hero_header_tag_helper.rb
@@ -3,7 +3,7 @@ module HeroHeaderTagHelper
     image ||= object.hero_image_url || '/assets/banner.jpg'
     content_tag :header, capture(&block),
       class: [:hero, :baniereImageProject, options[:class], image],
-      style: "background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.6)), url(#{image}); background-repeat: no-repeat; background-position: center center; background-size: contain; background-color: #1a1a1a; min-height: 60dvh",
+      style: "background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.6)), url(#{image}); background-repeat: no-repeat; background-position: center center; background-size: cover; background-color: #1a1a1a; min-height: 60dvh",
       data: { 'image-url' => image_url(image) }
   end
 end
