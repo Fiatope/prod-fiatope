55acaf656bd8a16f5e5cbacf3fe8b24f43b859a8
Djouko <destructeurkratos@gmail.com>
2026-05-26 19:56:54 +0100

    Fix Fiatope migration version

--- db/migrate/20260525120000_add_registration_number_to_organizations.rb
+++ db/migrate/20260525120000_add_registration_number_to_organizations.rb
@@ -1,4 +1,4 @@
-class AddRegistrationNumberToOrganizations < ActiveRecord::Migration
+class AddRegistrationNumberToOrganizations < ActiveRecord::Migration[6.1]
   def change
     add_column :organizations, :registration_number, :string unless column_exists?(:organizations, :registration_number)
   end
